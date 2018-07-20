79c2784407f0d2c82d2d05502eabd55028722995
Fouad Boukredine <fouad.b.business@gmail.com>
2018-07-20 15:43:42 -0400

    Script reminder to take a break by playing a song

--- Take_A_Break/take_a_break.py
+++ Take_A_Break/take_a_break.py
@@ -1,7 +1,7 @@
 import webbrowser
 import time
 
-print("This program started on " + time.ctime())
+print("The reminder script started on " + time.ctime())
 for i in range(3):
     time.sleep(10)
     webbrowser.open("https://www.youtube.com/watch?v=jaRsJvZgg2s")
