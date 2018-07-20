c5be243f83794a3206c7ac20c23784c82e259e9d
Fouad Boukredine <fouad.b.business@gmail.com>
2018-07-20 14:04:58 -0400

    Script to rename files using functions

--- Secret_Message/secret_message.py
+++ Secret_Message/secret_message.py
@@ -15,5 +15,6 @@ def rename_files():
         os.rename(file, file.translate(None, "0123456789"))
     # You need to return to the original directory using the chdir() function from the os module
     # Just replace the bellow "your original directory" with your actual original directory
-    os.chdir("your original directory")
+    os.chdir("put your original directory here")
+
 rename_files()
